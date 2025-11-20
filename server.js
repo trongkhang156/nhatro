e25302d0ab65680c55651472593ca3db3e2d6896
trongkhang156 <132425547+trongkhang156@users.noreply.github.com>
2025-11-20 11:29:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -250,6 +250,5 @@ app.get('/api/history', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
