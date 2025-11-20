d13fa45467f3d3ec4a871bff6c9b7b69cfedf89a
trongkhang156 <132425547+trongkhang156@users.noreply.github.com>
2025-11-20 12:32:54 +0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,10 @@
-const CACHE_NAME = "room-billing-cache-v1";
-const urlsToCache = ["/","/index.html","/manifest.json"];
+const CACHE_NAME = 'nhatro-cache-v1';
+const urlsToCache = ['/', '/index.html'];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(
-    keys.map(k=>k!==CACHE_NAME?caches.delete(k):null)
-  )));
-});
-
-self.addEventListener("fetch", e=>{
-  e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
